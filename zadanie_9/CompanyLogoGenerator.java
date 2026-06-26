3ffb7e6a6177ab8cb913c099a656d14e0643d647
btbd1 <annchest1@gmail.com>
2026-06-26 10:23:19 +0300

    убраны подписи к выводам

--- zadanie_9/CompanyLogoGenerator.java
+++ zadanie_9/CompanyLogoGenerator.java
@@ -17,12 +17,8 @@ public class CompanyLogoGenerator {
   };
 
   public static void main(String[] args) throws Exception {
-    // Установка кодировки для вывода
-    System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
-
 
     // Генерация логотипа компании
-
     String surname1 = getRandomSurname();
     String ampersand = "&";
     Faker faker = new Faker();
@@ -33,15 +29,9 @@ public class CompanyLogoGenerator {
     System.out.println(FigletFont.convertOneLine(surname2));
 
     // ===== Генерация юридических данных =====
-    System.out.println("~ РЕГИСТРАЦИОННЫЕ ДАННЫЕ ~");
-    System.out.println();
-
-    String fullName = faker.name().fullName();
-    System.out.println("ФИО: " + fullName);
-    String phoneNumber = faker.phoneNumber().phoneNumber();
-    System.out.println("Телефон: " + phoneNumber);
-    String address = faker.address().fullAddress();
-    System.out.println("Адрес: " + address);
+    System.out.println(faker.name().fullName());
+    System.out.println(faker.phoneNumber().phoneNumber());
+    System.out.println(faker.address().fullAddress());
   }
 
   // метод получения случайной фамилии из массива
