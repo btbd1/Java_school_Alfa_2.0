d0909d58ceeaf677ac02f2812578de2ed646b70d
btbd1 <annchest1@gmail.com>
2026-06-29 17:30:37 +0300

    Добавлен файл CoffeeMachine.java

---
+++ src/zadanie_11/CoffeeMachine.java
@@ -0,0 +1,26 @@
+package ru.alfabank.homework11;
+
+public class CoffeeMachine {
+
+  private static int minimumWaterAmount = 200;
+
+  public void makeCoffee (int ml){
+    if (ml <= minimumWaterAmount){
+      throw new NotEnoughWaterException(
+          "В кофемашине недостаточно воды. Доступно: "
+              + ml + " мл. Добавьте не менее: "
+              + (minimumWaterAmount - ml + 1) + " мл.");
+    }
+
+    System.out.println("Кофе приготовлен");
+
+  };
+
+  public int calculateCups (int ml, int cupCapacity){
+    return cupCapacity / ml;
+  };
+
+  public void printCoffeeName (String coffeeName){
+    System.out.println(coffeeName.toUpperCase());
+  };
+}
